8c935a74e8f56b9eafd7a0ee6d9b81994f1c4747
ppparkta <86940801+ppparkta@users.noreply.github.com>
2022-11-03 17:15:54 +0900

    um...

---
+++ Libft/ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sooyang <sooyang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/09 02:00:09 by sooyang           #+#    #+#             */
+/*   Updated: 2022/07/11 20:05:54 by sooyang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"libft.h"
+
+size_t	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	size_t	i;
+	size_t	d;
+	size_t	s;
+
+	d = ft_strlen(dest);
+	s = ft_strlen(src);
+	if (size <= d)
+		return (s + size);
+	i = 0;
+	while (i < size - 1 - d && src[i])
+	{
+		dest[d + i] = src[i];
+		i++;
+	}
+	dest[d + i] = 0;
+	return (d + s);
+}
