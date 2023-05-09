400e9d23eb2aa78f8d140c0781e819a9a8f05258
ppparkta <pppppark2@gmail.com>
2023-05-09 16:47:25 +0900

    fix: 철학자가 죽고나서도 다른 철학자가 동작하는 문제 해결

--- philosopher/philo/src/take_fork.c
+++ philosopher/philo/src/take_fork.c
@@ -6,20 +6,34 @@
 /*   By: sooyang <sooyang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 23:30:27 by sooyang           #+#    #+#             */
-/*   Updated: 2023/05/09 15:25:59 by sooyang          ###   ########.fr       */
+/*   Updated: 2023/05/09 16:46:19 by sooyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-void pick_up_fork(t_philo *philo)
+void	pick_up_fork(t_philo *philo)
 {
 	pthread_mutex_lock(philo->left_fork);
-	printf("%lld %d has taken a left fork\n",
-		get_time_table(philo->table->start_time), philo->philo_num);
+	pthread_mutex_lock(&philo->table->print);
+	pthread_mutex_lock(&philo->table->m_is_dead);
+	if (philo->table->is_dead == 0)
+	{
+		printf("%lld %d has taken a left fork\n",
+			get_time_table(philo->table->start_time), philo->philo_num);
+	}
+	pthread_mutex_unlock(&philo->table->m_is_dead);
+	pthread_mutex_unlock(&philo->table->print);
 	pthread_mutex_lock(philo->right_fork);
-	printf("%lld %d has taken a right fork\n",
-		get_time_table(philo->table->start_time), philo->philo_num);
+	pthread_mutex_lock(&philo->table->print);
+	pthread_mutex_lock(&philo->table->m_is_dead);
+	if (philo->table->is_dead == 0)
+	{
+		printf("%lld %d has taken a right fork\n",
+			get_time_table(philo->table->start_time), philo->philo_num);
+	}
+	pthread_mutex_unlock(&philo->table->m_is_dead);
+	pthread_mutex_unlock(&philo->table->print);
 }
 
 void put_down_fork(t_philo *philo)
