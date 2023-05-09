f736c32eca970dc735790de7089a721aa1da1267
ppparkta <pppppark2@gmail.com>
2023-05-10 00:17:25 +0900

    feat: philo 최종 마무리, norm 점검

--- philosopher/philo/src/take_fork.c
+++ philosopher/philo/src/take_fork.c
@@ -6,7 +6,7 @@
 /*   By: sooyang <sooyang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 23:30:27 by sooyang           #+#    #+#             */
-/*   Updated: 2023/05/09 22:09:59 by sooyang          ###   ########.fr       */
+/*   Updated: 2023/05/09 23:17:39 by sooyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,8 @@ void	pick_up_fork(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->table->all_fork[philo->left_fork]);
 	print_msg(philo, " has taken a fork\n");
-	//pthread_mutex_unlock(&philo->table->m_is_dead);
 	pthread_mutex_lock(&philo->table->all_fork[philo->right_fork]);
-	//pthread_mutex_lock(&philo->table->m_is_dead);
-	//if (philo->table->is_dead == 0)
-		print_msg(philo, " has taken a fork\n");
-	//pthread_mutex_unlock(&philo->table->m_is_dead);
+	print_msg(philo, " has taken a fork\n");
 }
 
 void	put_down_fork(t_philo *philo)
--- philosopher/philo/src/utils.c
+++ philosopher/philo/src/utils.c
@@ -6,11 +6,23 @@
 /*   By: sooyang <sooyang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 02:14:33 by sooyang           #+#    #+#             */
-/*   Updated: 2023/05/09 21:43:33 by sooyang          ###   ########.fr       */
+/*   Updated: 2023/05/10 00:13:48 by sooyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (i < str[i])
+	{
+		i++;
+	}
+	return (i);
+}
 
 int	ft_atoi(const char *str)
 {
@@ -19,6 +31,8 @@ int	ft_atoi(const char *str)
 
 	i = 0;
 	ans = 0;
+	if (ft_strlen(str) >= 11)
+		return (0);
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
 	if (str[i] == '-' )
