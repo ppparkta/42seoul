ddd16efaa55b3f22e235b5201304d0cd9ddc78c7
ppparkta <pppppark2@gmail.com>
2023-05-10 13:34:52 +0900

    fix: 반영 안 된 부분 제출

--- philosopher/philo/src/take_fork.c
+++ philosopher/philo/src/take_fork.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   take_fork.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sooyang <sooyang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: sooyang <sooyang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 23:30:27 by sooyang           #+#    #+#             */
-/*   Updated: 2023/05/09 23:17:39 by sooyang          ###   ########.fr       */
+/*   Updated: 2023/05/10 13:34:18 by sooyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 void	pick_up_fork(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->table->all_fork[philo->left_fork]);
-	print_msg(philo, " has taken a fork\n");
+	if (check_dead(philo->table))
+		print_msg(philo, " has taken a fork\n");
 	pthread_mutex_lock(&philo->table->all_fork[philo->right_fork]);
-	print_msg(philo, " has taken a fork\n");
+	if (check_dead(philo->table))
+		print_msg(philo, " has taken a fork\n");
 }
 
 void	put_down_fork(t_philo *philo)
