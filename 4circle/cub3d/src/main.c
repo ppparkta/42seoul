7838acec412378d68fa94f5afb947db3a2e9505a
ppparkta <pppppark2@gmail.com>
2023-07-18 23:25:07 +0900

    feat: 창 생성하기

--- 4circle/cub3d/src/main.c
+++ 4circle/cub3d/src/main.c
@@ -6,13 +6,19 @@
 /*   By: sooyang <sooyang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 22:09:02 by sooyang           #+#    #+#             */
-/*   Updated: 2023/07/18 23:19:11 by sooyang          ###   ########.fr       */
+/*   Updated: 2023/07/18 23:24:42 by sooyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include "../mlx/mlx.h"
 
 int main(){
-	
-	return (0);
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "연습용 창");
+	(void)*win_ptr;
+	mlx_loop(mlx_ptr);
 }
