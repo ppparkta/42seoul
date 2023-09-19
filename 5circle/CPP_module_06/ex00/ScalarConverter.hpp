dc7a5a0788289a91e0117d45b3e67365fe201bed
ppparkta <pppppark2@gamil.com>
2023-09-20 04:37:28 +0900

    feat: cpp module 06 ex00 개념 이해 후 코드 대대적인 수정; convertChar, convertString 뼈대 만들고 StringTo~ 함수로 뺌 #4

--- 5circle/CPP_module_06/ex00/ScalarConverter.hpp
+++ 5circle/CPP_module_06/ex00/ScalarConverter.hpp
@@ -5,17 +5,20 @@
 #include <iostream>
 #include <string>
 #include <limits>
-#include <climits>
+#include <cstdlib>
+#include <cctype>
+#include <sstream>
+#include <iomanip>
 
 class ScalarConverter {
 	private:
-
-	public:
 		ScalarConverter();
-		ScalarConverter(std::string lit);
-		ScalarConverter(const ScalarConverter &copy);
-		ScalarConverter &operator=(const ScalarConverter &assign);
+		ScalarConverter(const ScalarConverter &scalar);
+		ScalarConverter &operator=(const ScalarConverter &scalar);
 		~ScalarConverter();
+	public:
+		static void convert(std::string target);
+		
 };
 
 #endif
