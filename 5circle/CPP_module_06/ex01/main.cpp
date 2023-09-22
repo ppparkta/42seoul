769462b09681cfe609bd28884e09d4881449191b
ppparkta <pppppark2@gmail.com>
2023-09-22 11:24:17 +0900

    fix: cpp06 ex01 testcase 추가 #4

--- 5circle/CPP_module_06/ex01/main.cpp
+++ 5circle/CPP_module_06/ex01/main.cpp
@@ -9,6 +9,11 @@ int main()
 	data->str = "aaa";
 	data->number = 10;
 
+	std::cout << "Before Data_ptr: " << data << std::endl;
+	std::cout << "uint_ptr: " << Serializer::serialize(data) << std::endl;
+	std::cout << "After Data_ptr: " << Serializer::deserialize(Serializer::serialize(data)) << std::endl
+			  << std::endl;
+
 	std::cout << "test str: " << Serializer::deserialize(Serializer::serialize(data))->str << std::endl;
 	std::cout << "test nb: " << Serializer::deserialize(Serializer::serialize(data))->number << std::endl;
 
