dec4b3e7ff155c41422e7f407b5b7501fe777e58
ppparkta <pppppark2@gmail.com>
2023-09-24 21:23:06 +0900

    feat: ex00 완성 및 함수템플릿 개념정리

---
+++ 5circle/CPP_module_07/ex00/Whatever.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef __WHATEVER_HPP__
+#define __WHATEVER_HPP__
+
+template <typename T>
+void swap(T &n1, T &n2){
+	T tmp;
+	tmp = n1;
+	n1 = n2;
+	n2 = tmp;
+}
+
+template <typename T>
+const T &min(T &n1, T &n2){
+	if (n1 < n2)
+		return n1;
+	else
+		return n2;
+}
+
+template <typename T>
+const T &max(T &n1, T &n2){
+	if (n1 > n2)
+		return n1;
+	else
+		return n2;
+}
+
+#endif
--- 5circle/CPP_module_07/ex02/main.cpp
+++ 5circle/CPP_module_07/ex02/main.cpp
@@ -1,5 +1,5 @@
-#include ""
+
 
 int main(){
-	
+
 }
