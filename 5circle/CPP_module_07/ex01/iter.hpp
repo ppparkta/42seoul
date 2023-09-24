855324ff0a25813d9ba3b895092494bd6887e515
ppparkta <pppppark2@gmail.com>
2023-09-24 21:35:35 +0900

    feat: ex01 iter 함수템플릿 제작 #5

--- 5circle/CPP_module_07/ex01/iter.hpp
+++ 5circle/CPP_module_07/ex01/iter.hpp
@@ -2,6 +2,14 @@
 #ifndef __ITER_HPP__
 #define __ITER_HPP__
 
-
+template <typename T>
+void iter(T *arr, size_t size, void (*function)(T &target)){
+	if (arr == NULL || size == 0 || f == NULL){
+		return ;
+	}
+	for (size_t i = 0; i < size; i++){
+		function(arr[i]);
+	}
+}
 
 #endif
