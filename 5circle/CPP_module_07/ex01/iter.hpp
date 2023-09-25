a3a3ada00c7b9404d8a0e7fd9d411c00e5573a08
ppparkta <pppppark2@gmail.com>
2023-09-26 00:02:40 +0900

    feat: cpp07 ex01 main문 및 테스트용 template 함수 추가 #5

--- 5circle/CPP_module_07/ex01/iter.hpp
+++ 5circle/CPP_module_07/ex01/iter.hpp
@@ -2,14 +2,21 @@
 #ifndef __ITER_HPP__
 #define __ITER_HPP__
 
+#include <iostream>
+
 template <typename T>
-void iter(T *arr, size_t size, void (*function)(T &target)){
-	if (arr == NULL || size == 0 || f == NULL){
-		return ;
-	}
-	for (size_t i = 0; i < size; i++){
+void iter(T *arr, size_t size, void (*function)(T &target))
+{
+	if (arr == NULL || size == 0 || function == NULL)
+		return;
+	for (size_t i = 0; i < size; i++)
 		function(arr[i]);
-	}
+}
+
+template <typename T>
+void test_func(T &target)
+{
+	target += 2;
 }
 
 #endif
--- 5circle/CPP_module_07/ex01/main.cpp
+++ 5circle/CPP_module_07/ex01/main.cpp
@@ -1,5 +1,34 @@
 #include "iter.hpp"
 
-int main(){
+void func1(char &target)
+{
+	target = std::toupper(static_cast<char>(target));
+}
 
+int main()
+{
+	{
+		std::cout << "test1" << std::endl;
+		char arr[3] = {'i', 'v', 'e'};
+		::iter(arr, 3, func1);
+		std::cout << "arr[0]: " << arr[0] << std::endl;
+		std::cout << "arr[1]: " << arr[1] << std::endl;
+		std::cout << "arr[2]: " << arr[2] << std::endl << std::endl;
+	}
+	{
+		std::cout << "test2" << std::endl;
+		int arr[3] = {0, 1, 2};
+		::iter(arr, 3, test_func);
+		std::cout << "arr[0]: " << arr[0] << std::endl;
+		std::cout << "arr[1]: " << arr[1] << std::endl;
+		std::cout << "arr[2]: " << arr[2] << std::endl << std::endl;
+	}
+	{
+		std::cout << "test3" << std::endl;
+		char arr[3] = {'i', 'v', 'e'};
+		::iter(arr, 3, test_func);
+		std::cout << "arr[0]: " << arr[0] << std::endl;
+		std::cout << "arr[1]: " << arr[1] << std::endl;
+		std::cout << "arr[2]: " << arr[2] << std::endl << std::endl;
+	}
 }
