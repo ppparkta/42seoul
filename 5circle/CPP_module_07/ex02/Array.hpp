86bc1f5ead383a7da0e6f80cd7fa2e095641a89e
ppparkta <pppppark2@gmail.com>
2023-09-26 15:45:12 +0900

    feat: cpp07 ex02 operator[] 제외 class template 구현 #5

---
+++ 5circle/CPP_module_07/ex02/Array.hpp
@@ -0,0 +1,69 @@
+#pragma once
+#ifndef __ARRAY_HPP__
+#define __ARRAY_HPP__
+
+#include <iostream>
+#include <exception>
+
+template <typename T>
+class Array
+{
+	private:
+		T *_arr;
+		unsigned int _size;
+
+	public:
+		Array() : _arr(NULL), _size(0) {}
+		Array(unsigned int n) : _arr(new T[n]), _size(n) {}
+		Array(const Array &array)
+		{
+			this->_size = array.size();
+			this->_arr = new T[array.size()];
+			for (unsigned int i = 0; i < array.size(); i++)
+				this->_arr[i] = array[i];
+		}
+		Array &operator=(const Array &array)
+		{
+			if (this->_arr != NULL)
+				delete[] this->_arr;
+			this->_size = array.size();
+			this->_arr = new T[array.size()];
+			for (unsigned int i = 0; i < array.size(); i++)
+				this->_arr[i] = array[i];
+			return *this;
+		}
+		~Array()
+		{
+			delete[] this->_arr;
+		}
+		T &operator[](unsigned int index)
+		{
+		}
+		T &operator[](unsigned int index) const
+		{
+		}
+		unsigned int size() const
+		{
+			return this->_size;
+		}
+		class OutOfBoundsException : public exception
+		{
+		public:
+			virtual const char *what() const throw()
+			{
+				return "Index is out of bounds!";
+			}
+		};
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, Array<T> const &arr)
+{
+	for (unsigned int i = 0; arr.size(); i++)
+	{
+		os << "index " << i << ": " arr[i] << std::endl;
+	}
+	return os;
+}
+
+#endif
--- 5circle/CPP_module_07/ex02/main.cpp
+++ 5circle/CPP_module_07/ex02/main.cpp
@@ -1,5 +1,16 @@
-
+#include "Array.hpp"
 
 int main(){
+	{
+		std::cout<<"test1"<<std::endl;
 
+	}
+	{
+		std::cout<<"test2"<<std::endl;
+		
+	}
+	{
+		std::cout<<"test3"<<std::endl;
+		
+	}
 }
