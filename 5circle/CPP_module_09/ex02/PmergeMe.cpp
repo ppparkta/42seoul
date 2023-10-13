ac3dfb9c99a6cf39f3b32d16d780503c8e992396
ppparkta <pppppark2@gmail.com>
2023-10-13 21:48:07 +0900

    feat: cpp09 ex02 짜잘 수정

--- 5circle/CPP_module_09/ex02/PmergeMe.cpp
+++ 5circle/CPP_module_09/ex02/PmergeMe.cpp
@@ -30,42 +30,49 @@ void PmergeMe::merge_vector(const std::vector<size_t> &args)
 		this->pendingElements_vec.push_back(args.back());
 }
 
-void PmergeMe::insertion_sort_vec(){
+void PmergeMe::insertion_sort_vec()
+{
 	initialize_index();
-	if (!this->pendingElements_vec.empty()) 
+	if (!this->pendingElements_vec.empty())
 		this->mainChain_vec.insert(this->mainChain_vec.begin(), this->pendingElements_vec[0]);
 	int i = 1;
-	while(i < this->pendingElements_vec.size()){
+	while (i < this->pendingElements_vec.size())
+	{
 		int num = this->index.back();
-		//std::cout<<"index number = "<<num<<std::endl;
+		// std::cout<<"index number = "<<num<<std::endl;
 		int j = (i + num < this->pendingElements_vec.size()) ? i + num : this->pendingElements_vec.size();
-		for (int k = i; i <= --j ; k++) {
-			//std::cout<<"i = "<<i<<", k = "<<k<<", j = "<<j<<", pendingE[j] = "<<this->pendingElements_vec[j]<<std::endl;
-			this->mainChain_vec.insert(std::upper_bound(this->mainChain_vec.begin(), this->mainChain_vec.begin() + j + k, this->pendingElements_vec[j]),\
-				this->pendingElements_vec[j]);
-			//print_vector();
+		for (int k = i; i <= --j; k++)
+		{
+			// std::cout<<"i = "<<i<<", k = "<<k<<", j = "<<j<<", pendingE[j] = "<<this->pendingElements_vec[j]<<std::endl;
+			this->mainChain_vec.insert(std::upper_bound(this->mainChain_vec.begin(), this->mainChain_vec.begin() + j + k, this->pendingElements_vec[j]),
+									   this->pendingElements_vec[j]);
+			// print_vector();
 		}
 		i += num;
 		find_next_insertion();
 	}
 }
 
-void PmergeMe::print_vector(){
-	std::cout<<"=================================="<<std::endl;
-	std::cout<<"(vector) mainChain: ";
-	for(int i=0;i<this->mainChain_vec.size();i++){
-		std::cout<<this->mainChain_vec[i]<<" ";
+void PmergeMe::print_vector()
+{
+	std::cout << "==================================" << std::endl;
+	std::cout << "(vector) mainChain: ";
+	for (int i = 0; i < this->mainChain_vec.size(); i++)
+	{
+		std::cout << this->mainChain_vec[i] << " ";
 	}
-	std::cout<<std::endl;
-	std::cout<<"(vector) pending_e: ";
-	for(int i=0;i<this->pendingElements_vec.size();i++){
-		std::cout<<this->pendingElements_vec[i]<<" ";
+	std::cout << std::endl;
+	std::cout << "(vector) pending_e: ";
+	for (int i = 0; i < this->pendingElements_vec.size(); i++)
+	{
+		std::cout << this->pendingElements_vec[i] << " ";
 	}
-	std::cout<<std::endl;
-	std::cout<<"=================================="<<std::endl;
+	std::cout << std::endl;
+	std::cout << "==================================" << std::endl;
 }
 
-void PmergeMe::merge_insertion_sort_vec(const std::vector<size_t> &args){
+void PmergeMe::merge_insertion_sort_vec(const std::vector<size_t> &args)
+{
 	merge_vector(args);
 	insertion_sort_vec();
 }
@@ -99,31 +106,36 @@ void PmergeMe::merge_list(const std::vector<size_t> &args)
 		this->pendingElements_lst.push_back(args.back());
 }
 
-void PmergeMe::insertion_sort_lst(){
+void PmergeMe::insertion_sort_lst()
+{
 	initialize_index();
-	if (!pendingElements_lst.empty()){
+	if (!pendingElements_lst.empty())
+	{
 		mainChain_lst.insert(mainChain_lst.begin(), pendingElements_lst.front());
 		pendingElements_lst.pop_front();
 	}
 	int now = 1;
-	while (!pendingElements_lst.empty()){
+	while (!pendingElements_lst.empty())
+	{
 		int bind_point = (index.back() < pendingElements_lst.size()) ? index.back() : pendingElements_lst.size();
 		find_next_insertion();
 		now += bind_point;
 
 		std::list<size_t> tmp;
-		while (bind_point--){
+		while (bind_point--)
+		{
 			tmp.push_front(pendingElements_lst.front());
 			pendingElements_lst.pop_front();
 		}
 
 		std::list<size_t>::iterator iter = mainChain_lst.begin();
-		for(int i = 0; i < now; i++)
+		for (int i = 0; i < now; i++)
 			iter++;
 
 		now += tmp.size();
-		
-		while (!tmp.empty()){
+
+		while (!tmp.empty())
+		{
 			std::list<size_t>::iterator insert_e = std::upper_bound(mainChain_lst.begin(), iter, tmp.front());
 			mainChain_lst.insert(insert_e, tmp.front());
 			tmp.pop_front();
@@ -133,33 +145,40 @@ void PmergeMe::insertion_sort_lst(){
 	}
 }
 
-void PmergeMe::print_list(){
-	std::cout<<"=================================="<<std::endl;
-	std::cout<<"(list) mainChain: ";
-	for(std::list<size_t>::iterator i = this->mainChain_lst.begin(); i != this->mainChain_lst.end(); i++){
-		std::cout<<*i<<" ";
+void PmergeMe::print_list()
+{
+	std::cout << "==================================" << std::endl;
+	std::cout << "(list) mainChain: ";
+	for (std::list<size_t>::iterator i = this->mainChain_lst.begin(); i != this->mainChain_lst.end(); i++)
+	{
+		std::cout << *i << " ";
 	}
-	std::cout<<std::endl;
-	std::cout<<"(list) pending_e: ";
-	for(std::list<size_t>::iterator i = this->pendingElements_lst.begin(); i != this->pendingElements_lst.end(); i++){
-		std::cout<<*i<<" ";
+	std::cout << std::endl;
+	std::cout << "(list) pending_e: ";
+	for (std::list<size_t>::iterator i = this->pendingElements_lst.begin(); i != this->pendingElements_lst.end(); i++)
+	{
+		std::cout << *i << " ";
 	}
-	std::cout<<std::endl;
+	std::cout << std::endl;
+	std::cout << "==================================" << std::endl;
 }
 
-void PmergeMe::merge_insertion_sort_lst(const std::vector<size_t> &args){
+void PmergeMe::merge_insertion_sort_lst(const std::vector<size_t> &args)
+{
 	merge_list(args);
 	insertion_sort_lst();
 }
 
-void PmergeMe::initialize_index(){
+void PmergeMe::initialize_index()
+{
 	this->index.clear();
 	this->index.push_back(1);
 	this->index.push_back(3);
 }
 
-void PmergeMe::find_next_insertion(){
-	this->index.push_back(index[index.size()-1] + (2 * index[index.size() - 2]));
+void PmergeMe::find_next_insertion()
+{
+	this->index.push_back(index[index.size() - 1] + (2 * index[index.size() - 2]));
 }
 
 static void check_args(int argc, char **argv, std::vector<size_t> &args)
@@ -200,15 +219,16 @@ void PmergeMe::merge(int argc, char **argv)
 	{
 		std::cout << save_args[i] << " ";
 		if (i == save_args.size() - 1)
-			std::cout<<std::endl;
+			std::cout << std::endl;
 	}
 
 	sort(args.begin(), args.end());
-	std::cout<<"After: ";
-	for(int i=0;i<args.size();i++){
+	std::cout << "After: ";
+	for (int i = 0; i < args.size(); i++)
+	{
 		std::cout << args[i] << " ";
 		if (i == args.size() - 1)
-			std::cout<<std::endl;
+			std::cout << std::endl;
 	}
 
 	clock_t start_vec, finish_vec, start_lst, finish_lst;
@@ -221,6 +241,6 @@ void PmergeMe::merge(int argc, char **argv)
 	merge_insertion_sort_lst(save_args);
 	finish_lst = clock();
 
-	std::cout<<"Time to process a range of "<< save_args.size() << " elements with std::vector : "<<finish_vec-start_vec<<" ms"<<std::endl;
-	std::cout<<"Time to process a range of "<< save_args.size() << " elements with std::list : "<<finish_lst-start_lst<<" ms"<<std::endl;
+	std::cout << "Time to process a range of " << save_args.size() << " elements with std::vector : " << finish_vec - start_vec << " ms" << std::endl;
+	std::cout << "Time to process a range of " << save_args.size() << " elements with std::list : " << finish_lst - start_lst << " ms" << std::endl;
 }
