e0d20eaf592f0cf9adacec89a64ad5172c4f5536
ppparkta <pppppark2@gmail.com>
2023-10-20 22:07:51 +0900

    feat: ft_irc 허용함수 공부 중 #17

--- 5circle/ft_irc/main.cpp
+++ 5circle/ft_irc/main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 
+/***
+ * socket, 
+ * close, 
+ * setsockopt, 
+ * getsockname, 
+ * getprotobyname, 
+ * gethostbyname, 
+ * getaddrinfo, 
+ * freeaddrinfo, 
+ * bind, 
+ * connect, 
+ * listen, 
+ * accept, 
+ * htons, 
+ * htonl, 
+ * ntohs, 
+ * ntohl, 
+ * inet_addr, 
+ * inet_ntoa, 
+ * send, 
+ * recv, 
+ * signal, 
+ * sigaction, 
+ * lseek, 
+ * fstat, 
+ * fcntl, 
+ * poll 
+ ***/
+
 int main(int argc, char **argv){
 
 }
