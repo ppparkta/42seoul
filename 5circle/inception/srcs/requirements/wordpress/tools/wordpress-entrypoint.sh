60d392b6952fcd1882c0b88898d017c4c082e89a
ppparkta <pppppark2@gmail.com>
2024-03-21 21:09:17 +0900

    fix: 인셉션 makefile, docker-compose 수정

--- 5circle/inception/srcs/requirements/wordpress/tools/wordpress-entrypoint.sh
+++ 5circle/inception/srcs/requirements/wordpress/tools/wordpress-entrypoint.sh
@@ -6,7 +6,7 @@ mkdir -p /run/php
 
 wp core download --locale=en_US --force --allow-root
 wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb --allow-root
-wp core install --url="https://localhost" --admin_user="$WORDPRESS_ADMIN_NAME" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --skip-email --title="$WORDPRESS_TITLE" --allow-root
+wp core install --url="https://sooyang.42.fr" --admin_user="$WORDPRESS_ADMIN_NAME" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --skip-email --title="$WORDPRESS_TITLE" --allow-root
 wp user create "$WORDPRESS_USER_NAME" "$WORDPRESS_USER_EMAIL" --user_pass="$WORDPRESS_USER_PASSWORD" --allow-root
 wp theme activate twentytwentytwo --allow-root
 
